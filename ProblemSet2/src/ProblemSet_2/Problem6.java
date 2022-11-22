e63b2e50ab16375cb537e30735ebccd3dccd2d3e
charliehigginsiii <charliehiggiii@gmail.com>
2022-11-22 16:58:59 -0500

    Commit

--- ProblemSet2/src/ProblemSet_2/Problem6.java
+++ ProblemSet2/src/ProblemSet_2/Problem6.java
@@ -1,5 +1,7 @@
 package ProblemSet_2;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -8,17 +10,17 @@ import java.util.LinkedList;
 import java.util.Set;
 
 public class Problem6 {
-	
-	public LinkedHashMap sortHashmap(HashMap hp) {
-		LinkedHashMap newHashmap=new LinkedHashMap<>();
-		for(Object key:newHashmap.keySet()) {
-			
+	public LinkedHashMap sortHashmap(HashMap hashmap) {
+		LinkedHashMap orderedmap=new LinkedHashMap();
+		ArrayList list=new ArrayList(hashmap.values());
+		for(int i=0;i<list.size();i++) {
+			for(Object key:hashmap.keySet()) {
+				if(hashmap.get(key)==list.get(i)) {
+					orderedmap.put(key, list.get(i));
+				}
+			}
 		}
-		
-		
-		
-		return newHashmap;
-		
+		return orderedmap;
 	}
 	public static void main(String[]args) {
 		HashMap hashmap=new HashMap();
@@ -26,6 +28,12 @@ public class Problem6 {
 		hashmap.put("Random2", 1);
 		hashmap.put("Random3", 19);
 		hashmap.put("Random4", 2);
+		LinkedHashMap orderedmap=new Problem6().sortHashmap(hashmap);
+		Iterator iterate=orderedmap.entrySet().iterator();
+		while(iterate.hasNext()) {
+			System.out.println(iterate.next());
+		}
+		
 	}
 
 }
