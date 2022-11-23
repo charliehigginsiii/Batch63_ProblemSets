c6a7882f13e95d28bcd7b8943acbd8ba9407a3a3
charliehigginsiii <charliehiggiii@gmail.com>
2022-11-23 10:02:43 -0500

    Commit

---
+++ ProblemSetNovember21st/src/ProblemSet1/Problem4.java
@@ -0,0 +1,12 @@
+package ProblemSet1;
+
+public class Problem4 {
+	public String isEvenorOdd(int a) {
+		String evenodd=(a%2==0)?"Number is even":"Number is odd";
+		return evenodd;
+	}
+	public static void main(String[]args) {
+		System.out.println(new Problem4().isEvenorOdd(8));
+	}
+
+}
