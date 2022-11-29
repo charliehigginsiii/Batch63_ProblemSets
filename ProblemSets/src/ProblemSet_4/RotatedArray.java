c394df5b57ea5f07ce6c41657021284f40e5521a
charliehigginsiii <charliehiggiii@gmail.com>
2022-11-29 00:44:21 -0500

    Commit

--- ProblemSets/src/ProblemSet_4/RotatedArray.java
+++ ProblemSets/src/ProblemSet_4/RotatedArray.java
@@ -1,7 +1,7 @@
 package ProblemSet_4;
 
 public class RotatedArray {
-	static void Rotate(int []arr, int numberOfRotations )
+	public void rotate(int []arr, int numberOfRotations )
 	{
 	    for(int i=0;i<numberOfRotations;i++) {
 	    	int tempvalue=arr[0];
@@ -14,10 +14,8 @@ public class RotatedArray {
 	}
 	 
 	    public static void main (String[] args) {
-	        int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
-	 
-	        // Function calling
-	        Rotate(arr, 2);
+	        int arr[] = {1,8,9,4,5,6,15};
+	        new RotatedArray().rotate(arr, 2);
 	        for (int i = 0; i < arr.length; i++) {
 		        System.out.print(arr[i]+" ");
 		    }
